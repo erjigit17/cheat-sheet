1a21c3113d2e26ad58a3b807e57ffce4f8bffb4d
Эржигит Имамов <erjigit17@gmail.com>
2021-09-08 05:31:24 +0600

    add compose and pipe

---
+++ pipe-compose.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const campose = (...fns) => x => fns.reverse().reduce((v, f)  => f(v), x)
+const pipe = (...fns) => x => fns.reduce((v, f)  => f(v), x)
+// Usage
+
+const upperFirst = (word) => word.charAt(0).toUpperCase() + word.slice(1)
+const upperCapital = (s) => s.split(' ').map(upperFirst).join(' ')
+const lower = (s) => s.toLowerCase()
+
+const s = 'ErjigiT IMAMOV'
+console.log(s)
+console.log(`lower('${s}') = '${lower(s)}'`)
+console.log(`upperCapital('${s}') = '${upperCapital(s)}'`)
+
+
+{
+  console.log('Use compose')
+  const capitalize = campose(upperCapital, lower)
+  console.log(`capitalize('${s}') = '${capitalize(s)}'`)
+}
+
+{
+  console.log('Use pipe')
+  const capitalize = pipe(lower, upperCapital)
+  console.log(`capitalize('${s}') = '${capitalize(s)}'`)
+}
