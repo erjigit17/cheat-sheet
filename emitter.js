cf8247d2e404fe5183e3236ed071e4912f7eac63
Эржигит Имамов <erjigit17@gmail.com>
2021-09-07 18:48:47 +0600

    done

---
+++ emitter.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const events = require('events')
+
+const emitter = () => {
+  const ee = new events.EvantsEmmiter()
+  const emit = ee.emit
+  ee.emit = (...args) => {
+    if (args[0] !== '*' (emit.apply(ee, args)))
+    args.unshift('*')
+    emit.apply(ee, args)
+  }
+  return ee
+}
+
+module.exports = emitter
+
+
+
