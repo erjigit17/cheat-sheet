e78b6db659d876c0ce75c42720035eb81af8c9b7
Эржигит Имамов <erjigit17@gmail.com>
2021-09-13 19:41:36 +0600

    add context

--- iife.js
+++ iife.js
@@ -9,3 +9,27 @@ for (var i = 0; i < fib.length; i++) {
     }, 1000)
   })(i)
 }
+
+//
+let result = []
+for (var i = 0; i < 5; i++) {
+  result.push( function() {
+    console.log(i)})
+}
+
+result[2]()
+result[4]()
+
+//******
+let result2 = []
+for (var i = 0; i < 5; i++) {
+  (function(){
+    var j = i
+    result2.push( function() {
+      console.log(j)})
+  })()
+}
+
+result2[2]()
+result2[4]()
+
